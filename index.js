b2d2e8716db466085e2dd0ba967cd4830c35c99f
marinarezkalla <marina.saad704@gmail.com>
2024-05-19 15:24:56 +0300

    Cart api

--- index.js
+++ index.js
@@ -4,24 +4,19 @@ const express = require ('express');
 
 const productRouter = require ('./routes/products.route')
 
-<<<<<<< HEAD
+
 const cartRouter = require('./routes/cart.route')
 
-=======
->>>>>>> 8befa86869b956183724a4cbfb4dc59a2092520e
 const app = express();
 
 app.use(express.json())
 
 const port = process.env.PORT || 3006;
-
 app.use('/api/products', productRouter);
 
-<<<<<<< HEAD
+
 app.use('/api/cart', cartRouter);
 
-=======
->>>>>>> 8befa86869b956183724a4cbfb4dc59a2092520e
 
 app.listen(port, ()=>{
     console.log(" I am listening");
